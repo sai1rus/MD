152762ca31549372dca800901cf833494bee631e
Rustam <1111rus1111@gmail.com>
2025-08-14 17:49:39 +0500

    Objects

--- lesson_32/scripts.js
+++ lesson_32/scripts.js
@@ -1,125 +1,113 @@
 // Задание 1
 
-function greet() {
-    console.log("Привет, мир!")
+let person = {
+    name: "Рустам",
+    age: "34",
+    city: "Караганда"
 }
 
-greet()
+console.log(person)
 
 
 // Задание 2
 
-function sayHello(name) {
-    console.log(`Привет, ${name}!`)
-}
+console.log(person["name"], person.age)
 
-sayHello("Рустам")
-sayHello("Александр")
-sayHello("Азамат")
+person.city = "Алмата"
+console.log(person.city)
 
 
 // Задание 3
 
-const sum = function(a, b) {
-    return a + b
+person.greet = function() {
+    console.log(`Привет, меня зовут ${this.name}, мне ${this.age} лет(года)!`)
 }
 
-console.log(sum(2, 3))
-console.log(sum(22, 11))
-console.log(sum(30, 40))
+person.greet()
 
 
 // Задание 4
 
-const isEven = function(x) {
-    return x % 2 === 0
+let obj1 = {
+    a: 10,
+    b: 20
 }
 
-console.log(isEven(9))
-console.log(isEven(4))
-console.log(isEven(20))
+let obj2 = {
+    a: 10,
+    b: 20
+}
+
+console.log(obj1 == obj2) // false
+console.log(obj1 === obj2) // false
+
+/**
+ *  В обоих случаях ответ - false, т.к. сравнение идет по ссылкам,
+ * а не по внутренним свойствам и в памяти это разные объекты
+ */
 
 
 // Задание 5
 
-function max(a, b) {
-    if (a > b) {
-        return a
-    } else if (b > a) {
-        return b
-    } else {
-        return "Числа равны"
+let book = {
+    title: 'Title',
+    author: 'Author',
+    details: {
+        year: 2011,
+        pages: 1500
     }
 }
 
-console.log(max(2, 3))
-console.log(max(6, 3))
-console.log(max(3, 3))
+let copyBook = Object.assign({}, book)
+
+book.details.year = 2025
+
+console.log(copyBook.details.year) // 2025
+console.log(copyBook.details.year) // 2025
+
+/**
+ * Изменение во вложенном объекте повлияло на копию, потому что
+ * `Object.assign()` копирует объект поверхностно, свойства копируются
+ * по значению только у верхнего уровня, а вложенные объкты копируются по ссылке,
+ * соответсвенно изменения во вложенном объекте будут одиникаво передаваться и копии,
+ * и оригиналу.
+ */
 
 
 // Задание 6
 
-function getInitials(firstName, lastName) {
-    const in1 = prompt("Введите имя:").charAt(0).toUpperCase()
-    const in2 = prompt("Введите фамилию:").charAt(0).toUpperCase()
-    return `${in1}.${in2}.`
+let calculator = {
+    a: 4,
+    b: 6,
+    sum: function() {
+        return this.a + this.b
+    },
+    multiply: function() {
+        return this.a * this.b
+    }
 }
 
-console.log(getInitials())
+console.log(calculator.sum()) // 4
+console.log(calculator.multiply()) // 24
 
 
 // Задание 7
 
-const square = function(n) {
-    return n * n
+const car = {
+    brand: "Brand",
+    model: "Model"
 }
 
-const cube = function(n) {
-    return square(n) * n
-}
+car.brand = "newBrand"
 
-console.log(square(2))
-console.log(cube(2))
-console.log(square(3))
-console.log(cube(3))
+console.log(car.brand) // newBrand
 
 
-// Задание 8
-
-const sumAr = (a, b) => a + b
-
-console.log(sumAr(20, 10))
-
-
-// Дополнительное задание
-
-
-function getFactorial(x) {
-    if (x > 1) {
-        return x * getFactorial(x - 1)
-    } else {
-        return 1
-    }
-}
-
-console.log(getFactorial(5))
-console.log(getFactorial(0))
-console.log(getFactorial(1))
-console.log(getFactorial(7))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Изменение удалось сделать, потому что const
+ * не позволяет переопределять или переназнать объект,
+ * но при этом допускает изменения содержимого объекта.
+ */
 
 
 
