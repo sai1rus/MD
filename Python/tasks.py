6d267f91a8d7db05c1842468b548da61d0d475fe
Rustam <1111rus1111@gmail.com>
2025-08-24 16:21:21 +0500

    +2 tasks

--- Python/tasks.py
+++ Python/tasks.py
@@ -11,3 +11,25 @@ for i in range(1, n+1):
         p = 15
 
 print(f'{t//60}:{t%60}')
+
+
+'''Делимость'''
+
+n = int(input())
+m= int(input())
+
+if max(n, m) % min(n, m) == 0:
+    print(1)
+else:
+    print(0)
+
+'''Стоимость покупки'''
+
+A = int(input())
+B = int(input())
+N = int(input())
+
+S = (A * 100 + B) * N
+
+print(f"{S // 100} {S % 100}")
+
